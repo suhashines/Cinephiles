a17db117e3c28a0292da05c47a37107d9978fbf8
prithu-anan <prithua00@gmail.com>
2023-08-16 00:21:00 +0600

    hudai

--- movies/src/index.js
+++ movies/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import axios from 'axios';
-import {Provider} from 'react-redux';
-import {store} from './store';
+// import {Provider} from 'react-redux';
+// import {store} from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL = "http://localhost:5000/";
