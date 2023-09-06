21eba41044df3a3e4edfb404206947bf053ed333
prithu-anan <prithua00@gmail.com>
2023-09-06 21:47:50 +0600

    changePassword done

--- movies/src/Bookings/Bookings.js
+++ movies/src/Bookings/Bookings.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getMovieById } from '../api-helpers/api-helpers';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 
 const Bookings = () => {
     const [movie, setMovie] = useState([]);
@@ -18,11 +18,11 @@ const Bookings = () => {
   return (    
     <Box
         display={"flex"}
-        flexDirection={"row"}
-        justifyContent={"center"}
+        flexDirection={"column"}
+        justifycontents={"center"}
         alignItems={"center"}        
         width={"100vw"}
-        height={"165vh"}
+        height={"265vh"}
         margin={"auto"}
         // marginTop={4}
     >
@@ -33,7 +33,7 @@ const Bookings = () => {
         justifyContent={"left"}
         alignItems={"left"}        
         width={"98%"}
-        height={"40%"}
+        height={"66vh"}
         margin={"auto"}
         marginTop={0}
         // padding={1}
@@ -45,8 +45,8 @@ const Bookings = () => {
           // marginLeft={4}
           style={{
             position:"absolute",
-            left:"8%",
-            top:"55%", 
+            left:"15%",
+            top:"65%", 
             zIndex:2,
             border:"solid",
             borderColor:"white",
@@ -54,26 +54,150 @@ const Bookings = () => {
         }} 
           src={movie[0]?.POSTER_URL} 
           alt={movie?.TITLE} 
-          width={"30%"} 
-          height={"70%"}
+          width={"22%"} 
+          height={"85%"}
         >
         </img>
         <img
           justifyContent={"center"}
           margin={"auto"}
-          style={{position:"absolute", zIndex:1}} 
+          // marginLeft={40}
+          style={{
+            position:"absolute",
+            left:"10%", 
+            zIndex:1
+        }} 
           src={movie[0]?.BACK_POSTER_URL} 
           alt={movie?.TITLE} 
-          width={"70%"} 
-          height={"100%"}>
+          width={"80%"} 
+          height={"90%"}>
         </img>
+        <Typography
+          style={{            
+            position:"absolute",
+            left:"39%", 
+            top:"95%", 
+            zIndex:3,
+            color:"#7c4699",
+            fontWeight:"bold",
+            fontSize:"35px",
+            whiteSpace: "wrap", // Add this property to allow text wrapping
+            maxWidth: "45%" // Set a maximum width to control wrapping behavior
+            // transform:"translate(0%, 0%)"
+          }}
+          variant='h6'
+        >
+          {movie[0]?.TITLE}
+        </Typography>
+        <Box
+          style={{
+            position:"absolute",
+            left:"48%", 
+            top:"127%", 
+            zIndex:3,
+            transform:"translate(-50%, -50%)"
+            // color:"white",
+            
+          }}
+        >
+          <Typography
+          style={{
+            // fontWeight:"bold",
+            fontSize:"20px"
+            
+          }}            
+          >
+          <b>Release Date:</b> {new Date(movie[0]?.RELEASE_DATE).toDateString()}
+          </Typography>
+          <Typography
+          style={{
+            // fontWeight:"bold",
+            fontSize:"20px",
+            
+          }}            
+          >
+          <b>Genre:</b> {new Array(movie[0]?.GENRE).toString()}
+          </Typography>
+          <Typography
+            style={{
+              // fontWeight:"bold",
+              fontSize:"20px",            
+            }}            
+          >
+          <b>Actors:</b> {new Array(movie[0]?.ACTOR).toString()}
+          </Typography>
+          <Typography
+            style={{
+              // fontWeight:"bold",
+              fontSize:"20px",            
+            }}            
+          >
+          <b>Director:</b> {movie[0]?.DIRECTOR}
+          </Typography>
+          <Typography
+            style={{
+              // fontWeight:"bold",
+              fontSize:"20px",            
+            }}            
+          >
+          <b>Duration:</b> {movie[0]?.DURATION} minutes
+          </Typography>
+          <Button 
+              variant={"outlined"} 
+              sx={{
+                position:"absolute",
+                left:"0%",
+                top:"110%",
+                margin:"auto", 
+                color:"white", 
+                bgcolor:"#7c4699", 
+                fontSize:"12px", 
+                borderColor:"#7c4699",
+                width:"100%",
+                '&:hover': {
+                  backgroundColor: '#900c3f', 
+                  borderColor: '#900c3f', 
+                  color:"#e3e4e6"}}}                
+          >
+              Book
+          </Button>
+        </Box>
       </Box>
       {/* <Box>
         <Typography variant='h4'>
           {movie[0]?.TITLE}
         </Typography>
       </Box> */}
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifycontent={"center"}
+        // alignItems={"center"}        
+        width={"70%"}
+        height={"100vh"}
+        margin={"auto"}
+        marginTop={-15}
+        padding={1}
+        // borderRadius={10}
+      >
+        <Typography
+          color={"#7c4699"}
+          textAlign={"left"}
+          fontWeight={"bold"} 
+          variant='h5'
+        >
+          SYNOPSIS
+        </Typography>
+        <Typography 
+          variant='h5'
+          marginTop={2}
+          // textAlign={"center"}
+        >
+          {movie[0]?.SYNOPSIS}
+        </Typography>
+      </Box>
     </Box>
+    
   )
 }
 
--- movies/src/components/Profile/ChangePass.js
+++ movies/src/components/Profile/ChangePass.js
@@ -1,9 +1,55 @@
-import { Box, Button, FormLabel, TextField } from '@mui/material'
-import React from 'react'
+import { Box, Button, FormLabel, TextField, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { changePassword } from '../../api-helpers/api-helpers'
 
 const ChangePass = () => {
+    const labelStyle = {mt:1, mb:1}
+
+    const [inputs, setInputs] = useState({
+        u_id:localStorage.getItem('userId'),
+        oldPassword:"",
+        newPassword:"",
+        confirmPassword:""
+    })
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = (e) => {
+        setInputs((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    useEffect(()=>{
+        setMessage(message);
+    },[message])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let changeSuccess;
+
+        try{
+            changeSuccess = await changePassword(inputs);
+            setMessage(changeSuccess.message);
+        }catch(err){
+            console.log(err);
+        }
+
+        if(changeSuccess.success){
+            setInputs({
+                oldPassword: '',
+                newPassword: '',
+                confirmPassword: '',
+              });
+        }
+        
+        // console.log("Change", changeSuccess);
+    };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <Box
             display={"flex"}
             flexDirection={"column"}
@@ -15,31 +61,31 @@ const ChangePass = () => {
             marginTop={10}
             // padding={6}
             >
-            <FormLabel>Old Password</FormLabel>
+            <FormLabel sx={{labelStyle}}>Old Password</FormLabel>
             <TextField
-                // value={inputs.password}
-                // onChange={handleChange}
-                sx={{bgcolor:'white'}} 
+                value={inputs.oldPassword}
+                onChange={handleChange}
+                sx={{bgcolor:'white', width:'60%'}}
                 variants={"standard"} 
                 margin={"normal"}  
                 type={"password"} 
-                name="old password"
+                name="oldPassword"
             />
-            <FormLabel>New Password</FormLabel>
+            <FormLabel sx={labelStyle}>New Password</FormLabel>
             <TextField
-                // value={inputs.password}
-                // onChange={handleChange}
-                sx={{bgcolor:'white'}} 
+                value={inputs.newPassword}
+                onChange={handleChange}
+                sx={{bgcolor:'white', width:'60%'}} 
                 variants={"standard"} 
                 margin={"normal"}  
                 type={"password"} 
-                name="new password"
+                name="newPassword"
             />
-            <FormLabel>Confirm Password</FormLabel>
+            <FormLabel sx={labelStyle}>Confirm Password</FormLabel>
             <TextField
-                // value={inputs.confirmPassword}
-                // onChange={handleChange}
-                sx={{bgcolor:'white'}} 
+                value={inputs.confirmPassword}
+                onChange={handleChange}
+                sx={{bgcolor:'white', width:'60%'}} 
                 variants={"standard"} 
                 margin={"normal"}  
                 type={"password"} 
@@ -47,7 +93,7 @@ const ChangePass = () => {
             />
             
             <Button 
-                sx={{mt:2, borderRadius:10, bgcolor:"#2b2d42", color:"white",
+                sx={{mt:2, borderRadius:10, bgcolor:"#2b2d42", color:"white", marginTop:3,
                 '&:hover': {backgroundColor: '#7c4699', borderColor: '#7c4699', color:"#e3e4e6"}}} 
                 type='submit'
                 variant={"contained"}
@@ -55,7 +101,11 @@ const ChangePass = () => {
                 // width={"200%"}
                 >
                     {"Confirm Submission"}
-            </Button>                                
+            </Button>
+
+            <Typography variant="h6" color={'red'} textAlign={"center"} marginTop={2}>
+                {message}
+            </Typography>                                
         </Box>
     </form>
   )
